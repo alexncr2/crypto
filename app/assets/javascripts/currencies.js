b3e8a26a253ca279f2b6cf6f7852a1e5d08a5561
alexncr2 <alex.nicoara3@gmail.com>
2018-05-11 15:11:30 +0300

    graph flicker removed

--- app/assets/javascripts/currencies.js
+++ app/assets/javascripts/currencies.js
@@ -1,13 +1,17 @@
 
 $(document).ready(function(){
 
+	var chart;
+	var values, old_values;
 
+	function readValues(funct){
+		values = $.get("currencies/generate_chart", funct);
+		return values;
+	}
 
-
-	$.get("currencies/generate_chart", function(data) {
-
-		//console.log("in generate");  
-		var chart = c3.generate({
+	//console.log("in generate");
+	function generateChart(data){
+		chart = c3.generate({
 			    data: {
 		        columns: data.currencies,
 		        type : 'donut'
@@ -16,15 +20,22 @@ $(document).ready(function(){
 		        title: "Your wallet"
 		    }
 		});
+	}
 
+	readValues(generateChart);
+	old_values = values;
 
-		setInterval(function () {
-			$.get("currencies/generate_chart", function(data) {
-				//console.log("in interval");  
-		    	chart.load({
-		        	columns: data.currencies
-		    	});
-		    });
-		}, 3000);
-	});
+	function updateChart(data){
+		chart.load({
+			columns: data.currencies
+		});
+	}
+
+	setInterval(function () {
+		if(values != old_values){
+			readValues(updateChart);
+			old_values = values;
+		}
+	}, 3000);
+
 });
